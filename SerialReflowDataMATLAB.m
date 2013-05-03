387d126969716f7707fe49c06af58e3a321c892f
Tyler H <tyler.hobbs@duke.edu>
2013-05-03 06:07:41 -0300

    Update SerialReflowDataMATLAB.m

--- SerialReflowDataMATLAB.m
+++ SerialReflowDataMATLAB.m
@@ -13,7 +13,7 @@ fclose(instrfind);
 s = serial('/dev/tty.usbmodemfa141')
 
 % Open the Serial buffer and pause to allow user time to start Reflow
-fopen(s); pause(25);
+fopen(s); pause(15);
 
 % Open the figure window and clear it
 figure(1); clf;
@@ -21,13 +21,30 @@ figure(1); clf;
 % Initialize the while variables
 d=0;
 k=0;
-
+A=0;
 while s.BytesAvailable >= 0
     d = fscanf(s,'%f %f %f');   % Read the data into variable, d
     d = d'                      % Transpose d
     if  d(1) ~= 0
+        k = k + 1;
+        A(k,[1 2 3]) = d;
+        
         hold on
-        plot(d(1),d(3),'k-')    % Plot the data
+        plot(d(1),d(3),'k-');    % Plot the data
+        plot(d(1),d(2),'b-');    % Plot the set Temp data
+        title('Temperature (C) vs. Time (s) for Reflow Run');
+        xlabel('Time (seconds)');
+        ylabel('Oven Temperature (Celsius)')
+        grid on;
+        legend('Actual Temp. Data','Set Temp. Data',0)
     end
 end
+
+%% Calculate the R squared
+SSresid = sum((A(:,3) - A(:,2)).^2);
+SStotal = sum((A(:,3) - mean(A(:,3))).^2);
+r_2 = 1 - SSresid./SStotal
+
+text(105,230,['R^2 = ',num2str(r_2)],'FontSize',16)
+
 fclose(s);  % Close the Serial buffer
